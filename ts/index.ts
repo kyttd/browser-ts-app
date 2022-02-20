1198f06e80d48d4e537b954bbbbb888721eadea1
kyttd <kuyt1992@gmail.com>
2022-02-20 15:17:33 +0900

    feat:: Taskクラスを作成し、inputの値をTaskのtitleとして取得できるようにした

--- ts/index.ts
+++ ts/index.ts
@@ -1,7 +1,29 @@
+import { EventListener } from "./EventListener";
+import { Task } from "./Task";
+
 class Application {
+  private readonly eventListener = new EventListener();
+
   start() {
-    console.log("hello world");
+    const createForm = document.getElementById("createForm") as HTMLElement;
+
+    this.eventListener.add(
+      "submit-handler",
+      "submit",
+      createForm,
+      this.handleSubmit
+    );
   }
+
+  private handleSubmit = (e: Event) => {
+    e.preventDefault();
+
+    const titleInput = document.getElementById("title") as HTMLInputElement;
+    if (!titleInput.value) return;
+
+    const task = new Task({ title: titleInput.value });
+    console.log(task);
+  };
 }
 
 window.addEventListener("load", () => {
