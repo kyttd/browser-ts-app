38ab20c98636fbd2c1eb97d775b92ea8bf20c818
kyttd <kuyt1992@gmail.com>
2022-02-27 16:46:15 +0900

    feat: localstorageの内容を画面描画する

--- ts/TaskRenderer.ts
+++ ts/TaskRenderer.ts
@@ -1,6 +1,7 @@
 import dragula from "dragula";
 
 import { Status, statusMap, Task } from "./Task";
+import { TaskCollection } from "./TaskCollection";
 
 export class TaskRenderer {
   constructor(
@@ -64,5 +65,41 @@ export class TaskRenderer {
     taskEl.append(spanEl, deleteButtonEl);
 
     return { taskEl, deleteButtonEl };
+  }
+
+  // 各statusのリストに対してrenderListを呼び出して画面描画
+  renderAll(taskColleciotn: TaskCollection) {
+    const todoTasks = this.renderList(
+      taskColleciotn.filter(statusMap.todo),
+      this.todoList
+    );
+    const doingTasks = this.renderList(
+      taskColleciotn.filter(statusMap.doing),
+      this.doingList
+    );
+    const doneTasks = this.renderList(
+      taskColleciotn.filter(statusMap.doing),
+      this.doneList
+    );
+
+    return [...todoTasks, ...doingTasks, ...doneTasks];
+  }
+
+  // 渡されたtasksのHTML要素を生成
+  private renderList(tasks: Task[], listEl: HTMLElement) {
+    if (tasks.length === 0) return [];
+
+    const taskList: Array<{
+      task: Task;
+      deleteButtonEl: HTMLButtonElement;
+    }> = [];
+
+    tasks.forEach((task) => {
+      const { taskEl, deleteButtonEl } = this.render(task);
+      listEl.append(taskEl);
+      taskList.push({ task, deleteButtonEl });
+    });
+
+    return taskList;
   }
 }
